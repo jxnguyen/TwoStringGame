a8b70b827fa506eec47bfe48297b0084afd7f48b
jxnguyen <johnxuannguyen@gmail.com>
2016-07-10 17:21:52 +0200

    implemented generateSubstrings()

--- Solution/Solution.java
+++ Solution/Solution.java
@@ -1,6 +1,39 @@
+import java.util.*;
+
+/*
+		- Generate all substrings of a String A and sort in ascending order.
+*/
 
 class Solution {
+
+	// MAIN
+	//
 	public static void main(String[] args) {
 
+		String[] strings = generateSubstrings("books");
+		for (String s : strings) {
+			System.out.println(s);
+		}
+	}
+
+	// GENERATE SUBSTRINGS | Return lexicographically sorted array of all
+	// 										 | substrings of A.
+	//
+	private static String[] generateSubstrings(String A) {
+
+		HashSet<String> substrings = new HashSet<String>();
+		// empty string
+		substrings.add("");
+		// for each start index
+		for (int i = 0; i < A.length(); i++)
+			// for each end index
+			for (int j = i+1; j <= A.length(); j++)
+				substrings.add(A.substring(i,j));
+
+		// convert to array & sort
+		String[] strings = new String[substrings.size()];
+		strings = substrings.toArray(strings);
+		Arrays.sort(strings);
+		return strings;
 	}
 }
